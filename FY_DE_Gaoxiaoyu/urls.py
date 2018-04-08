d638f797ee148c3c9343e72a1067031045866218
gaoxiaoyu1016 <963477295@qq.com>
2018-04-08 13:12:47 +0800

    新建一个Django项目

---
+++ FY_DE_Gaoxiaoyu/urls.py
@@ -0,0 +1,22 @@
+"""FY_DE_Gaoxiaoyu URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from cmdb import views
+urlpatterns = [
+   # path('admin/', admin.site.urls), 这个是admin后台的路由
+    path("index/",views.index),#这个是你的路由，重点是引号中的正则表达式和后面的业务函数。
+]
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\win7\PycharmProjects\FY_DE_Gaoxiaoyu\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
