2cb363df870626be4e3c1ea53fabc1c67266baf2
gaoxiaoyu1016 <963477295@qq.com>
2018-04-09 22:21:52 +0800

    第二次上传

--- cmdb/views.py
+++ cmdb/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("我爱老婆")
+   # return HttpResponse("Hello World")
+   # return render(request,"index.html",)
+    if request.method=="POST":
+        searchthings=request.POST.get("searchthings",None)
+        print(searchthings)
+    return render(request,"index.html",)
